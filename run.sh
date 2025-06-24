a5e151870860d092360a0822fb026b1f50358f30
electron271 <66094410+electron271@users.noreply.github.com>
2025-06-23 21:31:16 -0500

    guh

--- run.sh
+++ run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-mkdir -p cmake_build
-cd cmake_build
-cmake .. -DSERVER_ONLY=ON
+# Usage: ./run.sh [-b]    -b = build & install, otherwise just run
 
-# only install if there are changes
-if make -q; then
-    echo "No changes detected, skipping installation"
-else
+BUILD=false
+while getopts "b" opt; do
+  case $opt in
+    b) BUILD=true ;;
+  esac
+done
+
+if $BUILD; then
+  mkdir -p cmake_build
+  cd cmake_build
+
+  cmake .. -DSERVER_ONLY=ON
+
+  if make -q; then
+    echo "No changes detected, skipping build & install"
+  else
     make -j$(nproc --ignore=1)
     sudo make install
+  fi
+
+  cd ..
 fi
 
-cd ..
 supertuxkart --server-config=config.xml --network-console
